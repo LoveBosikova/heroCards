dfd631a7592633fb9e48d9e3efeeb472e24c184d
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-10-23 22:19:21 +0300

    added description, title, deleted unused things

--- heroapp/src/App.js
+++ heroapp/src/App.js
@@ -12,15 +12,15 @@ import LikedHeroesList from './components/LikedHeroesList/LikedHeroesList';
 function App() {
   return (
     <Router>
-    <div className="App">
-      <h1 className='App__title'>HeroLike</h1>
-      <p className='App__subtitle'>Like uor favorites, delete your hatred</p>
-      <button><Link to="/likedheroes">About</Link></button>
-      <Routes>
-          <Route path="/" element={<AllHeroesList />} />
-          <Route path="/likedheroes" element={<LikedHeroesList />}/>
-      </Routes>
-    </div>
+      <div className="App">
+        <h1 className='App__title'>HeroLike</h1>
+        <p className='App__subtitle'>Like uor favorites, delete your hatred</p>
+        <button><Link to="/likedheroes">About</Link></button>
+        <Routes>
+            <Route path="/" element={<AllHeroesList />} />
+            <Route path="/likedheroes" element={<LikedHeroesList />}/>
+        </Routes>
+      </div>
     </Router>
   );
 }
