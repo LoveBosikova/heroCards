4d9085e48e3f049e38eea03b7751d63a87bbeb8b
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-10-24 02:35:45 +0300

    redux partly done

--- heroapp/src/App.js
+++ heroapp/src/App.js
@@ -9,14 +9,47 @@ import {
   Link
 } from "react-router-dom";
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, compose } from 'redux';
 import AllHeroesList from './components/AllHeroesList/AllHeroesList';
 import LikedHeroesList from './components/LikedHeroesList/LikedHeroesList';
 import normalizeData from './normalizeData';
 
 const data = await axios.get("https://akabab.github.io/superhero-api/api/all.json");
 console.log(data.data);
-console.log(normalizeData(data.data));
+// console.log(normalizeData(data.data));
+
+const reducer = (state, action) => {
+
+  switch (action.type) {
+    case 'HERO__DELETE':
+      return _.omit(state, action.payload.id);
+    case 'HERO__LIKE':
+      const { id } = action.payload;
+      const hero = state.filter(u => u.id === id).map((hero)=> hero = {...hero, liked: true}); 
+      return {...state, ...hero};
+    case 'HERO__DISLIKE':
+      const curhero = state.filter(u => u.id === id).map((hero)=> hero = {...hero, liked: false}); 
+      curhero.liked = false;
+      console.log(curhero);
+      return {...state, ...curhero};
+    default: 
+      return state;
+  }
+};
+
+const store = createStore(reducer, normalizeData(data.data));
+console.log(store.getState())
+
+store.subscribe(() => console.log(store.getState()));
+
+const likeHero = (id) => ({ type: 'HERO__LIKE',  payload: { id } });
+const dislikeHero = (id) => ({ type: 'HERO__DISLIKE', payload: { id } });
+const deleteHero = (id) => ({ type: 'HERO__DELETE', payload: { id } });
+
+// // usage
+store.dispatch(likeHero(1)); 
+store.dispatch(dislikeHero(1)); 
+store.dispatch(deleteHero(1));
 
 function App() {
 
