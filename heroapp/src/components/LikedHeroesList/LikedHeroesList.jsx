3e4de2efac1e309c37b5e8b0ede5c0dfab11993e
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-10-26 17:35:18 +0300

    component likedHeroesList done

--- heroapp/src/components/LikedHeroesList/LikedHeroesList.jsx
+++ heroapp/src/components/LikedHeroesList/LikedHeroesList.jsx
@@ -1,3 +1,44 @@
-export default function LikedHeroesList (props) {
-    return <div className="likedHeroesList"></div>
+import { connect, useDispatch } from 'react-redux';
+import { getHeroes, getBoolFetching, getAllHeroes, likeHero, dislikeHero, deleteHero } from "../../redux/allHeroes-reducer";
+import { useEffect } from 'react';
+import Card from "../Card/Card";
+import Img from '../Img/Img';
+
+function AllHeroesList (props) {
+
+    useEffect(()=> {
+        props.getHeroes()
+    }, [])
+
+    const dispatch = useDispatch();
+
+    return (<div className="allHeroesList">
+        {props.isFetching ? <>Loader</>:<ul className="allHeroesList__cardsWrap">{props.heroes
+                                                                                            .filter(({isFavorite})=> isFavorite)
+                                                                                            .map((hero)=> {
+            return (<Card key={hero.id}>
+                <Card.Body>
+                    <Card.Name>Name: {hero.name}</Card.Name>
+                    {hero.alterEgos === ""? null : <Card.FullName>Full name: {hero.alterEgos}</Card.FullName>}
+                    {hero.occupation === "-"? null : <Card.Occupation>Occupation: {hero.occupation}</Card.Occupation>}
+                    <Card.Img>
+                        <Img src={hero.image} alt={hero.name}></Img>
+                    </Card.Img>
+                </Card.Body>
+                <Card.ButtonsWrap>
+                    {hero.isFavorite === true ? <Card.FullLike onClick={() => dispatch(dislikeHero(hero.id))}></Card.FullLike> : <Card.EmptyLike onClick={() => dispatch(likeHero(hero.id))}></Card.EmptyLike>}
+                    <Card.DeleteBtn onClick={() => dispatch(deleteHero(hero.id))}></Card.DeleteBtn>
+                </Card.ButtonsWrap>
+            </Card>)
+        })}</ul>}
+    </div>)
 }
+
+let mapStateToProps = (state) => {
+    return {
+    heroes: getAllHeroes(state),
+    isFetching: getBoolFetching(state),
+    }
+};
+
+export default connect(mapStateToProps, { getHeroes })(AllHeroesList);
